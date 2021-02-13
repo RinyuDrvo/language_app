48f3548f90ca8595478fbd6d609e2bcf0e068498
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-02-13 17:28:44 +0900

    fix global before guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,18 +35,11 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
   if (requiresAuth) {
-    // 認証状態を取得
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        next();
-      } else {
-        // 認証されていない場合
-        next({ name: 'Signin' });
-      }
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) next({ name: 'Signin' })
     })
-  } else {
-    next();
   }
+  next();
 })
 
 export default router

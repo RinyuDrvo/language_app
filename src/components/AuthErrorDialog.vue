f8fcb0291ae52776dd7038b6213f9c56cf49b4fe
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-02-23 16:52:29 +0900

    make Dialog component

---
+++ src/components/AuthErrorDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <Dialog :isShowModal="value">
+    <h1>Error</h1>
+    <p>{{ errorMessage }}</p>
+    <button @click="input">CLOSE</button>
+  </Dialog>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+import Dialog from "@/components/Dialog.vue";
+
+@Component({
+  components: {
+    Dialog,
+  },
+})
+export default class AuthErrorDialog extends Vue {
+  @Prop({ required: true }) errorMessage!: string;
+  @Prop({ required: true }) value!: boolean;
+
+  @Emit() input() {
+    return false;
+  }
+}
+</script>

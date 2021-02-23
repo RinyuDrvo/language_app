b33d08a1e536b1720cd4b03e0aa4cc79342b6d3f
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-02-23 17:00:07 +0900

    use dialog in home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,22 +2,40 @@
   <div class="hello">
     <h1>Home</h1>
     <button @click="logout">logout</button>
+
+    <auth-error-dialog
+      :errorMessage="errorMessage"
+      v-model="isShowErrorModal"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import firebase from 'firebase';
+import { Component, Vue } from "vue-property-decorator";
+import firebase from "firebase";
+import AuthErrorDialog from "@/components/AuthErrorDialog.vue";
 
-@Component
+@Component({
+  components: {
+    AuthErrorDialog,
+  },
+})
 export default class Home extends Vue {
+  errorMessage = "";
+  isShowErrorModal = false;
+
   logout() {
-    firebase.auth().signOut().then(() => {
-      // Sign out successful
-    }).catch((error) => {
-      // An error happened
-      throw new Error(error);
-    })
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign out successful
+      })
+      .catch((error) => {
+        // An error happened
+        this.errorMessage = error.message;
+        this.isShowErrorModal = true;
+      });
   }
 }
 </script>

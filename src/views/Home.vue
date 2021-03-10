184a3836f6f047d3fd0d88fb9548a004a0267138
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-03-10 09:41:17 +0900

    move home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import Home from "@/components/Home.vue";
+import Home from "@/components/pages/Home.vue";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({

1ff005474aa413fded47ba86609030ee2609e0a0
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-03-21 15:45:46 +0900

    delete unused prop

--- src/components/atoms/BaseTextbox.vue
+++ src/components/atoms/BaseTextbox.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 
 @Component
 export default class BaseTextbox extends Vue {

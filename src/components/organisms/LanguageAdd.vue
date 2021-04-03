dd4317524178358fa69d098e76e6e2ae999797ee
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-04-04 00:13:10 +0900

    add language

--- src/components/organisms/LanguageAdd.vue
+++ src/components/organisms/LanguageAdd.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="language-add">LANGUAGE ADD FORM</div>
+  <div class="language-add">
+    <LanguageAddForm
+      class="language-add__form"
+      :languageAddEvent="addLanguage"
+      v-model="language"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import LanguageAddForm from "@/components/molecules/LanguageAddForm.vue";
+import { LanguageModule } from "@/store/modules/LanguageStore";
+import { ROUTER_NAMES } from "@/constants/routerNames";
 
 @Component({
-  components: {},
+  components: {
+    LanguageAddForm,
+  },
 })
-export default class LanguageAdd extends Vue {}
+export default class LanguageAdd extends Vue {
+  language = "";
+
+  // 言語追加
+  addLanguage() {
+    LanguageModule.add(this.language);
+    this.$router.push({ name: ROUTER_NAMES.LANGUAGE_LIST });
+  }
+}
 </script>
 
 <style scoped lang="scss">

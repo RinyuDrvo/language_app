ad3bd2b447549abf7c3bb91796c7183bae8daa9c
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-04-04 18:49:56 +0900

    use validation error dialog

--- src/components/organisms/LanguageAdd.vue
+++ src/components/organisms/LanguageAdd.vue
@@ -9,10 +9,11 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 import LanguageAddForm from "@/components/molecules/LanguageAddForm.vue";
 import { LanguageModule } from "@/store/modules/LanguageStore";
 import { ROUTER_NAMES } from "@/constants/routerNames";
+import { Validation, ValidationErrors } from "@/utils/validationUtils";
 
 @Component({
   components: {
@@ -20,10 +21,45 @@ import { ROUTER_NAMES } from "@/constants/routerNames";
   },
 })
 export default class LanguageAdd extends Vue {
-  language = "";
+  /** エラーモーダル表示 */
+  @Prop({ required: true })
+  dialog!: (errorMessages: ValidationErrors) => Promise<unknown>;
 
-  // 言語追加
+  /** 追加する言語 */
+  private language = "";
+
+  private isShowValidationErrorDialog = false;
+  private validationErrorMessages: ValidationErrors = [];
+  private validation = new Validation();
+
+  mounted() {
+    // バリデーション情報のリセット
+    this.validation.reset();
+  }
+
+  validate(): boolean {
+    // 入力値の有無
+    this.validation.emptyInput<string>(this.language);
+    // 言語重複の有無
+    this.validation.redundantLanguage(LanguageModule.languages, this.language);
+
+    // エラー時モーダル表示
+    if (this.validation.isErrors) {
+      this.dialog(this.validation.validationErrors);
+
+      // バリデーション情報のリセット
+      this.validation.reset();
+
+      return false;
+    }
+
+    return true;
+  }
+
+  /** 言語追加 */
   addLanguage() {
+    if (!this.validate()) return;
+
     LanguageModule.add(this.language);
     this.$router.push({ name: ROUTER_NAMES.LANGUAGE_LIST });
   }
